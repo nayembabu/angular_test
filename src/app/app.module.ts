c6a1447a9949c9bbd6dcafa485553ad4e4c5392b
nayem <nayem11@yahoo.com>
2021-01-10 12:27:59 +0600

    add full file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopHeaderComponent } from './top-header/top-header.component';
+import { HeaderPartComponent } from './header-part/header-part.component';
+import { MenuComponent } from './menu/menu.component';
+import { SliderComponent } from './slider/slider.component';
+import { BodyPartComponent } from './body-part/body-part.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopHeaderComponent,
+    HeaderPartComponent,
+    MenuComponent,
+    SliderComponent,
+    BodyPartComponent,
+    FooterComponent
+    
   ],
   imports: [
     BrowserModule,
